99945707a1dd47cf4037efada1c252ac056d5d1d
harsha <hreshavena@gmail.com>
2026-07-14 10:09:26 +0530

    feat(auth): implement authentication routes and utilities

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,18 +1,13 @@
 import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-    res.send("signup route");
-});
-router.post("/login", (req, res) => {
-    res.send("login route");
-});
-router.post("/logout", (req, res) => {
-    res.send("logout route");
-});
-router.post("/register", (req, res) => {
-    res.send("register route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
+router.put("/update-profile", ProtectRoute, updateProfile)
 export { router as default };
+
+
