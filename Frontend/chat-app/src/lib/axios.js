4046439b90c4027f544389bf40a21c3ce0c3ab2e
harsha <hreshavena@gmail.com>
2026-07-19 11:04:23 +0530

    Update axios config

--- Frontend/chat-app/src/lib/axios.js
+++ Frontend/chat-app/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL:"https://localhost:5000/api",
-    withCredentails : true,
+    baseURL:"http://localhost:5000/api",
+    withCredentials : true,
 })
