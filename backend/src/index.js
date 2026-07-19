77d9ef548941638623d410385e1383c83a772a6c
harsha <hreshavena@gmail.com>
2026-07-19 11:04:24 +0530

    Update backend index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
   origin:"http://localhost:5173",
-  credentails:true,
+  credentials:true,
 }))
 
 app.use("/api/auth", authRoutes);
