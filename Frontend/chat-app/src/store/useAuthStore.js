49d006dcfa21b131d9aa578044d754d8a0b5367e
harsha <hreshavena@gmail.com>
2026-07-19 11:04:24 +0530

    Update auth store

--- Frontend/chat-app/src/store/useAuthStore.js
+++ Frontend/chat-app/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios"
+import toast from "react-hot-toast"
 
 export const useAuthStore = create((set)=>({
     authUser : null,
@@ -32,6 +33,31 @@ export const useAuthStore = create((set)=>({
       toast.error(error.response.data.message);
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+      get().disconnectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
     }
   },
   
