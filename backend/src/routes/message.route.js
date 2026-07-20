1a6f182e68171a73d19ad0cc59288a38925d6101
harsha <hreshavena@gmail.com>
2026-07-20 19:10:40 +0530

    feat: add hide-contact API endpoint

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectedRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSidebar, sendMessage, hideContact } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 
 router.get("/users", protectedRoute, getUsersForSidebar);
+router.post("/hide-contact/:id", protectedRoute, hideContact);
 router.get("/:id", protectedRoute, getMessages);
 router.post("/send/:id", protectedRoute, sendMessage);
 
